51d9c5a6805ce61122bbc26f4a1f8a603fef83a4
TeamPlayerGuy <101874781+TeamPlayerGuy@users.noreply.github.com>
2026-08-06 00:00:51 -0400

    Add no-subscription sources for Proxmox and Ceph

--- disable_proxmox_enterprise.sh
+++ disable_proxmox_enterprise.sh
@@ -1,11 +1,21 @@
-echo 'Enabled: false' >> /etc/apt/sources.list.d/pve-enterprise.sources
-
 CODENAME=$(grep VERSION_CODENAME /etc/os-release | tr -d '"' | cut -d= -f2)
 
-cat > /etc/apt/sources.list.d/pve-no-subscription.sources << 'EOF'
+echo 'Enabled: false' >> /etc/apt/sources.list.d/pve-enterprise.sources
+
+echo 'Enabled: false' >> /etc/apt/sources.list.d/ceph.sources
+
+cat > /etc/apt/sources.list.d/pve-no-subscription.sources << EOF
 Types: deb
 URIs: http://download.proxmox.com/debian/pve
 Suites: $CODENAME
 Components: pve-no-subscription
+Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
+EOF
+
+cat > /etc/apt/sources.list.d/ceph-no-subscription.sources << EOF
+Types: deb
+URIs: http://download.proxmox.com/debian/ceph-tentacle
+Suites: $CODENAME
+Components: no-subscription
 Signed-By: /usr/share/keyrings/proxmox-archive-keyring.gpg
 EOF
